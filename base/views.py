c324d6c49692d1ab0c55a6a1a3a209568500a894
nastaranxyousefi <nastaranxyousefi@gmail.com>
2022-04-29 14:08:18 +0430

    implementing necessary stuff for restrict user

--- base/views.py
+++ base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Task
 
@@ -15,30 +16,40 @@ class CustomLoginView(LoginView):
         return reverse_lazy('tasks')
 
 
-class TaskListView(generic.ListView):
+class TaskListView(LoginRequiredMixin, generic.ListView):
     model = Task
     template_name = 'base/task_list.html'
     context_object_name = 'tasks'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count()
+        return context
 
-class TaskDetailView(generic.DetailView):
+
+class TaskDetailView(LoginRequiredMixin, generic.DetailView):
     model = Task
     template_name = 'base/task_detail.html'
 
 
-class TaskCreateView(generic.CreateView):
+class TaskCreateView(LoginRequiredMixin, generic.CreateView):
     model = Task
     template_name = 'base/task_create_form.html'
-    fields = '__all__'
+    fields = ['title', 'description', 'complete',]
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreateView, self).form_valid(form)
 
 
-class TaskUpdateView(generic.UpdateView):
+class TaskUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Task
     template_name = 'base/task_update_form.html'
     fields = '__all__'
 
 
-class TaskDeleteView(generic.DeleteView):
+class TaskDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Task
     template_name = 'base/task_confirm_delete.html'
     success_url = reverse_lazy('tasks')
